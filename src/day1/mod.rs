753da8702d06f4adf2632aff5ac1c05321c3a4cd
Peter <ptriller@soapwars.de>
2023-12-01 10:39:45 +0100

    Day one dirty code

---
+++ src/day1/mod.rs
@@ -0,0 +1,80 @@
+use std::fs::read_to_string;
+use std::path::Path;
+
+fn extract_numner(line: &str) -> u32 {
+    let mut first = None;
+    let mut last = None;
+    for c in line.chars() {
+        if c.is_numeric() {
+            if first.is_none() {
+                first = Some(c.to_digit(10).unwrap())
+            }
+            last = Some(c.to_digit(10).unwrap())
+        }
+    }
+    first.unwrap() * 10 + last.unwrap()
+}
+
+
+fn extract_extended_number(line: &str) -> u32 {
+    let numbers: Vec<&str> = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let digits: Vec<&str> = vec!["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    let mut first = None;
+    let mut last = None;
+    for i in 0..line.len() {
+        let curr = &line[i..];
+        for j in 0..9 {
+            if curr.starts_with(numbers[j]) || curr.starts_with(digits[j]) {
+                let val = j as u32 + 1;
+                if first.is_none() {
+                    first = Some(val)
+                }
+                last = Some(val)
+            }
+        }
+    }
+    first.unwrap() * 10 + last.unwrap()
+}
+
+fn process_file(filename: &Path) -> u32 {
+    let mut result = 0;
+    for line in read_to_string(filename).unwrap().lines() {
+        let num = extract_numner(line);
+        assert_ne!(0, num);
+        result += num;
+    }
+    result
+}
+
+
+fn process_extended_file(filename: &Path) -> u32 {
+    let mut result = 0;
+    for line in read_to_string(filename).unwrap().lines() {
+        let num = extract_extended_number(line);
+        assert_ne!(0, num);
+        result += num;
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use crate::day1::{process_extended_file, process_file};
+
+    #[test]
+    fn do_problem1() {
+        let mut test_data = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        test_data.push("resources/day1/data1.txt");
+        println!("Result: {}", process_file(test_data.as_path()))
+    }
+
+    #[test]
+    fn do_problem2() {
+        let mut test_data = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        test_data.push("resources/day1/data1.txt");
+        println!("Result: {}", process_extended_file(test_data.as_path()))
+    }
+}
