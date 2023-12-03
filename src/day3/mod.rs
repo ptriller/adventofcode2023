893bbd2f3571a42eb56bb9e99f6a301157ad4c50
Peter Triller <ptriller@soapwars.de>
2023-12-03 18:15:46 +0100

    day3

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,7 +1,85 @@
+use std::collections::HashMap;
 use std::fs::read_to_string;
 use std::iter::Enumerate;
 use std::path::Path;
-use std::str::Chars;
+use std::str::{Chars};
+
+
+fn calc_gear_ratios(path: &Path) -> u32 {
+    let mut gears: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
+    let test_data = read_to_string(path).unwrap();
+    let data: Vec<&str> = test_data.lines().collect();
+    for (row, line) in data.iter().enumerate() {
+        let mut it = line.chars().enumerate();
+        while let Some((col, chr)) = it.next() {
+            if chr.is_ascii_digit() {
+                let mut num = chr.to_digit(10).unwrap();
+                let length = get_mumber(&mut num, &mut it);
+                GearChecker::check(&mut gears, &data, row, col, length, num);
+            }
+        }
+    }
+    let mut result = 0;
+    for (_k, v) in &gears {
+        if v.len() == 2 {
+            result += v.iter().product::<u32>();
+        }
+    }
+    result
+}
+
+struct GearChecker<'life> {
+    gears: &'life mut HashMap<(usize, usize), Vec<u32>>,
+    data: &'life Vec<&'life str>,
+    row: usize,
+    col: usize,
+    length: usize,
+    value: u32,
+}
+
+
+impl GearChecker<'_> {
+    fn check(gears: &mut HashMap<(usize, usize), Vec<u32>>,
+             data: &Vec<&str>,
+             row: usize,
+             col: usize,
+             length: usize,
+             value: u32) {
+        GearChecker {
+            gears,
+            data,
+            row,
+            col,
+            length,
+            value,
+        }.register_gears()
+    }
+
+    fn check_char(&mut self, chr: char, pos: (usize, usize)) {
+        if chr == '*' {
+            self.gears.entry(pos)
+                .and_modify(|v| v.push(self.value))
+                .or_insert(vec![self.value]);
+        }
+    }
+    fn register_gears(&mut self) {
+        let from = 0i32.max(self.col as i32 - 1) as usize;
+        let to = (self.data[0].len() - 1).min(self.col + self.length);
+        if self.row > 0 {
+            for (col, chr) in (from..=to).zip(&mut self.data[self.row - 1][from..=to].chars()) {
+                self.check_char(chr, (self.row - 1, col));
+            }
+        }
+        self.check_char(self.data[self.row].chars().nth(from).unwrap(), (self.row, from));
+        self.check_char(self.data[self.row].chars().nth(to).unwrap(), (self.row, to));
+        if self.row + 1 < self.data.len() {
+            for (col, chr) in (from..=to).zip(&mut self.data[self.row + 1][from..=to].chars()) {
+                self.check_char(chr, (self.row + 1, col));
+            }
+        }
+    }
+}
+
 
 fn calc_serial_number(path: &Path) -> u32 {
     let mut result = 0;
@@ -14,12 +92,10 @@ fn calc_serial_number(path: &Path) -> u32 {
                 let mut num = chr.to_digit(10).unwrap();
                 let length = get_mumber(&mut num, &mut it);
                 if is_serial(&data, row, col, length) {
-                    println!("Is Serial: {} {}", col, num);
                     result += num;
                 }
             }
         }
-        break
     }
     result
 }
@@ -30,15 +106,15 @@ fn is_symbol(chr: char) -> bool {
 
 fn is_serial(data: &Vec<&str>, row: usize, col: usize, length: usize) -> bool {
     let from = 0i32.max(col as i32 - 1) as usize;
-    let to = (data[0].len() - 1).min(col + 1 + length);
-    if row > 0 && data[row - 1][from..to].find(is_symbol).is_some() {
+    let to = (data[0].len() - 1).min(col + length);
+    if row > 0 && data[row - 1][from..=to].find(is_symbol).is_some() {
         return true;
     }
     if is_symbol(data[row].chars().nth(from).unwrap()) ||
         is_symbol(data[row].chars().nth(to).unwrap()) {
         return true;
     }
-    if row + 1 < data.len() && data[row + 1][from..to].find(is_symbol).is_some() {
+    if row + 1 < data.len() && data[row + 1][from..=to].find(is_symbol).is_some() {
         return true;
     }
     false
@@ -58,7 +134,7 @@ fn get_mumber(num: &mut u32, it: &mut Enumerate<Chars>) -> usize {
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
-    use crate::day3::calc_serial_number;
+    use crate::day3::{calc_gear_ratios, calc_serial_number};
 
     #[test]
     fn do_problem1() {
@@ -71,6 +147,6 @@ mod tests {
     fn do_problem2() {
         let mut test_data = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         test_data.push("resources/day3/input.txt");
-        println!("Day 3, Problem 1: Valid games: {}", test_data.as_path().to_str().unwrap());
+        println!("Day 3, Problem 1: Valid games: {}", calc_gear_ratios(test_data.as_path()));
     }
 }
